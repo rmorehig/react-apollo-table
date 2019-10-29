bb9a05286518e7fcb1d60f561af999db0d885e31
Rafael Moreno Higueras <rmh1516@gmail.com>
2019-10-29 10:15:03 +0100

    handle sorting

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
 import { useQuery } from "react-apollo";
 import Pagination from "./Pagination";
 import { FilterIcon } from "../Icons";
@@ -6,20 +6,44 @@ import Row from "./Row";
 
 const Table = ({ columns, query }) => {
   const { loading, error, data } = useQuery(query);
-
   const [activePage, setActivePage] = useState(1);
+
+  const sortingReducer = (state, action) => {
+    switch (action.type) {
+      case "SORT_DESC":
+        return "desc";
+      case "SORT_ASC":
+        return "asc";
+      default:
+        return state;
+    }
+  };
+  const initialSorting = "asc";
+  const [sorting, dispatch] = useReducer(sortingReducer, initialSorting);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [selected, setSelected] = useState([]);
+
   const handleActivePage = nextPage => {
     nextPage ? setActivePage(nextPage) : setActivePage(1);
   };
 
-  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const handleSorting = event => {
+    sorting === "asc"
+      ? dispatch({
+          type: "SORT_DESC"
+        })
+      : dispatch({
+          type: "SORT_ASC"
+        });
+  };
+
   const handleRowsPerPageChange = e => {
     setRowsPerPage(parseInt(e.target.value, 10));
     setActivePage(1);
   };
+
   const totalPages = data ? Math.ceil(data.entidades.length / rowsPerPage) : 1;
 
-  const [selected, setSelected] = useState([]);
   const numSelected = selected.length;
   const handleSelectAllClick = event => {
     if (event.target.checked) {
@@ -33,7 +57,6 @@ const Table = ({ columns, query }) => {
   const handleClick = (event, name) => {
     const selectedIndex = selected.indexOf(name);
     let newSelected = [];
-
     if (selectedIndex === -1) {
       newSelected = newSelected.concat(selected, name);
     } else if (selectedIndex === 0) {
@@ -46,13 +69,12 @@ const Table = ({ columns, query }) => {
         selected.slice(selectedIndex + 1)
       );
     }
-
     setSelected(newSelected);
   };
 
   const isSelected = name => selected.indexOf(name) !== -1;
 
-  if (loading) return "Cargando...";
+  if (loading) return "Loading...";
   if (error) return `Error! ${error.message}`;
   return (
     <div className="flex px-5 py-5 justify-center">
